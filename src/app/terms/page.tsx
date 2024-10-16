78c8bd73b1f1a6d35d69e3bb4e5c0f02411b8f0d
Oladejiraji <rajioladeji2@gmail.com>
2024-10-16 23:54:11 +0100

    pages fix

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -6,124 +6,53 @@ import MainAssets from "@/lib/assets/main";
 import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
 
-const AnimatedWords = ({ word, index }: { word: string; index: number }) => {
-  const elementRef = useRef<HTMLSpanElement>(null);
-  const [classes, setClasses] = useState("#848A8C");
-  useEffect(() => {
-    // Function to update the distance of the element from the top
-    const updateDistance = () => {
-      const scrollAtTop = window.scrollY === 0;
-      const scrolledToEnd =
-        window.innerHeight + window.scrollY >=
-        document.documentElement.scrollHeight - 1;
-      if (elementRef.current) {
-        const rect = elementRef.current.getBoundingClientRect();
-        if (scrolledToEnd) {
-          setClasses("white");
-          return;
-        }
-        if (scrollAtTop) {
-          setClasses("#848A8C");
-          return;
-        }
-        if (rect.top < 240) {
-          setClasses("white");
-        } else {
-          setClasses("#848A8C");
-        }
-      }
-    };
-
-    window.addEventListener("scroll", updateDistance);
-
-    return () => {
-      window.removeEventListener("scroll", updateDistance);
-    };
-  }, []);
-  return (
-    <span
-      ref={elementRef}
-      style={{ color: classes, transitionDelay: `${(index + 1) * 150}ms` }}
-      className="transition-all duration-500 animated_word"
-    >
-      {word}
-    </span>
-  );
-};
-
-const AnimatedLines = ({
-  line,
-  index,
-  lines,
-}: {
-  line: string;
-  lines: string[];
-  index: number;
-}) => {
-  const elementRef = useRef<HTMLSpanElement>(null);
-  const [classes, setClasses] = useState("#848A8C");
-  useEffect(() => {
-    // Function to update the distance of the element from the top
-    const updateDistance = () => {
-      console.log("scroll", index);
-      const scrollAtTop = window.scrollY === 0;
-      const scrolledToEnd =
-        window.innerHeight + window.scrollY >=
-        document.documentElement.scrollHeight - 1;
-      if (elementRef.current) {
-        const rect = elementRef.current.getBoundingClientRect();
-        if (scrolledToEnd) {
-          setClasses("white");
-          return;
-        }
-        if (scrollAtTop) {
-          setClasses("#848A8C");
-          return;
-        }
-        if (rect.top < 240) {
-          setClasses("white");
-        } else {
-          setClasses("#848A8C");
-        }
-      }
-    };
-
-    window.addEventListener("scroll", updateDistance);
-
-    return () => {
-      window.removeEventListener("scroll", updateDistance);
-    };
-  }, []);
-  return (
-    <span
-      ref={elementRef}
-      style={{ color: classes }}
-      className="transition-all duration-500"
-    >
-      {line}
-      {index < lines.length - 1 && <br />}
-    </span>
-  );
-};
-
-const AnimatedCharacters = ({ children }: { children: string }) => {
-  const lines = children.split("\n");
-
-  return (
-    <span className="whitespace-pre-wrap">
-      {lines.map((line, index) => (
-        // <AnimatedLines key={index} line={line} index={index} lines={lines} />
-        <p key={index}>{line}</p>
-      ))}
-    </span>
-  );
-};
-
 const Terms = () => {
+  const contentRef = useRef<HTMLDivElement>(null!);
+  const [wordsToShow, setWordsToShow] = useState(0);
+
+  const wrapTextInSpans = (node: HTMLDivElement | ChildNode) => {
+    node.childNodes.forEach((child) => {
+      if (child.nodeType === Node.TEXT_NODE) {
+        const words = child.textContent!.split(" ").map((word) => {
+          const span = document.createElement("span");
+          span.textContent = `${word} `;
+          span.classList.add("text-[#848A8C]");
+          span.classList.add("transition-all");
+          return span;
+        });
+        words.forEach((wordSpan) => {
+          node.insertBefore(wordSpan, child);
+        });
+        node.removeChild(child);
+      } else {
+        wrapTextInSpans(child);
+      }
+    });
+  };
+
+  const handleScroll = () => {
+    console.log("scroll");
+    const scrollTop = window.scrollY;
+    const docHeight =
+      document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = scrollTop / docHeight;
+
+    const spans = document.querySelectorAll("span");
+    const wordCount = Math.floor(scrollPercent * spans.length);
+    spans.forEach((span, index) => {
+      if (index < wordCount) {
+        span.classList.remove("text-[#848A8C]");
+        span.classList.add("text-white");
+      } else {
+        span.classList.remove("text-white");
+        span.classList.add("text-[#848A8C]");
+      }
+    });
+  };
+
   useEffect(() => {
-    const handleScroll = () => {
-      console.log("scroll");
-    };
+    const content = contentRef.current;
+    wrapTextInSpans(content);
 
     window.addEventListener("scroll", handleScroll);
 
@@ -132,98 +61,78 @@ const Terms = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
     <>
       <Header type={1} />
-      <div className="text-white max-w-[566px] mx-auto mt-[150px] mb-[20px] relative ">
+      <div
+        className="text-[#848A8C] max-w-[566px] mx-auto mt-[150px] mb-[20px] relative"
+        ref={contentRef}
+      >
         <div className="fixed top-0 left-0 h-screen w-screen bg-[rgba(0,0,0,0.5)] z-[-1]" />
         <div className="pb-6">
           <h1 className="text-[#8C8487] text-base font-geist-medium pb-3 ">
-            <AnimatedCharacters>Superbase</AnimatedCharacters>
+            Superbase
           </h1>
-          <h1 className="font-geist-bold text-2xl">
-            <AnimatedCharacters>Text and Privacy</AnimatedCharacters>
-          </h1>
+          <h1 className="font-geist-bold text-2xl">Text and Privacy</h1>
           <p className="font-geist-light text-base mix-blend-color">
-            <AnimatedCharacters>
-              Latest Revision: 3rd of June 2024
-            </AnimatedCharacters>
+            Latest Revision: 3rd of June 2024
           </p>
         </div>
 
         <div className="pb-6">
-          <h1 className="font-geist-medium text-[18px]">
-            <AnimatedCharacters>Introduction</AnimatedCharacters>
-          </h1>
+          <h1 className="font-geist-medium text-[18px]">Introduction</h1>
           <p className="font-geist-light text-[15px]">
-            <AnimatedCharacters>
-              Welcome to Superbase. Before you delve into and engage with our
-              comprehensive range of services, it&apos;s crucial to familiarize
-              yourself with and accept our Terms of Service and Privacy Policy.
-            </AnimatedCharacters>
+            Welcome to Superbase. Before you delve into and engage with our
+            comprehensive range of services, it&apos;s crucial to familiarize
+            yourself with and accept our Terms of Service and Privacy Policy.
+          </p>
+        </div>
+        <div className="pb-6">
+          <h1 className="font-geist-medium text-[18px]">Acceptance of Terms</h1>
+          <p className="font-geist-light text-[15px]">
+            By accessing or utilizing the Superbase Offerings, you are
+            confirming your understanding and unconditional acceptance of these
+            Terms and the accompanying Privacy Policy. Furthermore, you ensure
+            that you have the legal capacity and authority to enter into and be
+            bound by these Terms.
           </p>
         </div>
         <div className="pb-6">
           <h1 className="font-geist-medium text-[18px]">
-            <AnimatedCharacters>Acceptance of Terms</AnimatedCharacters>
+            Description of Our Services
           </h1>
           <p className="font-geist-light text-[15px]">
-            <AnimatedCharacters>
-              By accessing or utilizing the Superbase Offerings, you are
-              confirming your understanding and unconditional acceptance of
-              these Terms and the accompanying Privacy Policy. Furthermore, you
-              ensure that you have the legal capacity and authority to enter
-              into and be bound by these Terms.
-            </AnimatedCharacters>
+            Superbase, which is conveniently hosted at https://superbase.io,
+            offers users a state-of-the-art decentralized bridge specifically
+            designed for the smooth transfer of digital assets among a plethora
+            of other related services.
           </p>
         </div>
         <div className="pb-6">
           <h1 className="font-geist-medium text-[18px]">
-            <AnimatedCharacters>Description of Our Services</AnimatedCharacters>
-          </h1>
-          <p className="font-geist-light text-[15px]">
-            <AnimatedCharacters>
-              Superbase, which is conveniently hosted at https://superbase.io,
-              offers users a state-of-the-art decentralized bridge specifically
-              designed for the smooth transfer of digital assets among a
-              plethora of other related services.
-            </AnimatedCharacters>
-          </p>
-        </div>
-        <div className="pb-6">
-          <h1 className="font-geist-medium text-[18px]">
-            <AnimatedCharacters>
-              Accessing & Using Our Platform
-            </AnimatedCharacters>
+            Accessing & Using Our Platform
           </h1>
           <ul className="list-disc ml-4">
             <li className="font-geist-light text-[15px]">
-              <AnimatedCharacters>
-                Wallet Connection: For an enhanced experience, users can
-                seamlessly connect digital wallets, such as Metamask, to gain
-                direct access to Superbase Smart Contracts.
-              </AnimatedCharacters>
+              Wallet Connection: For an enhanced experience, users can
+              seamlessly connect digital wallets, such as Metamask, to gain
+              direct access to Superbase Smart Contracts.
             </li>
             <li className="font-geist-light text-[15px]">
-              <AnimatedCharacters>
-                Governance: The Operator diligently oversees the Platform&apos;s
-                functioning, but it&apos;s worth noting that Direct Usage
-                operates independently and outside this purview.
-              </AnimatedCharacters>
+              Governance: The Operator diligently oversees the Platform&apos;s
+              functioning, but it&apos;s worth noting that Direct Usage operates
+              independently and outside this purview.
             </li>
             <li className="font-geist-light text-[15px]">
-              <AnimatedCharacters>
-                Transaction Fees: Some transactions may carry associated fees.
-                Rest assured, users will always be informed and required to
-                consent prior to any charges being applied.
-              </AnimatedCharacters>
+              Transaction Fees: Some transactions may carry associated fees.
+              Rest assured, users will always be informed and required to
+              consent prior to any charges being applied.
             </li>
             <li className="font-geist-light text-[15px]">
-              <AnimatedCharacters>
-                Transaction Fees: Some transactions may carry associated fees.
-                Rest assured, users will always be informed and required to
-                consent prior to any charges being applied.
-              </AnimatedCharacters>
+              Transaction Fees: Some transactions may carry associated fees.
+              Rest assured, users will always be informed and required to
+              consent prior to any charges being applied.
             </li>
           </ul>
         </div>
